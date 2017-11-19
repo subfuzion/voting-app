5f8e81bf886b9017b68cf82fbceeab61a77fd3bc
Tony Pujals <tony@subfuzion.com>
2017-11-19 03:19:40 -0800

    improve voter error handling

--- src/voter/main.js
+++ src/voter/main.js
@@ -1,72 +1,129 @@
 const app = require('commander')
+const format = require('util').format
 const inquirer = require('inquirer')
 const pkg = require('./package.json')
 const request = require('r2')
 
-const voteURL = "http://localhost:3000/vote"
-const resultsURL = "http://localhost:3000/results"
-
-app
-  .version(pkg.version)
-
-app
-  .command('vote')
-  .description('vote for cats or dogs')
-  .action(onVote)
-
-app
-  .command('results')
-  .description('tally the votes')
-  .action(onResults)
-
-try {
-  app.parse(process.argv)
-} catch (err) {
-  onError(err)
+let apiOptions = {
+  host: process.env.VOTE_HOST || 'localhost',
+  port: process.env.VOTE_PORT || 3000
 }
 
-process.on('unhandledRejection', error => {
-  onError('(unhandledRejection)', error)
-})
+let apiURL = `http://${apiOptions.host}:${apiOptions.port}/`
+let voteURL = apiURL + 'vote/'
+let resultsURL = apiURL + 'results/'
 
-function onError(msg, err) {
-  console.log('error:', ...arguments)
+/**
+ * Log error message. Print 'error:' followed by remaining arguments,
+ * separated by spaces. If the first argument is a format string, then
+ * the remaining argument values will replace format string placeholders.
+ * @param {*} args Variable arguments
+ */
+function logError(...args) {
+  console.log('error:', format(...args))
 }
 
-// if no args, print help
-if (!process.argv.slice(2).length) {
-  app.outputHelp()
-  console.log()
+/**
+ * Exit process. 
+ * @param {*E} code Optional exit code (default: 0)
+ * @param {*} args Optional string arguments (first string can be a format string)
+ */
+function exit(code, ...args) {
+    if (typeof code === 'string') {
+      args.unshift(code)
+      code = 0
+    }
+  if (code === 0) {
+    console.log(format(...args))
+  } else {
+    logError(...args)
+  }
+  process.exit(code)
 }
 
-async function onVote(cmd, opts) {
-  let q = {
+// Handle the vote command and submit request to API.
+async function doVoteCmd(cmd, opts) {
+  // p holds the prompt settings
+  // p.filter is used to transform user-friendly prompt choices to
+  // the required values: 'cats' -> 'a',  'dogs' -> 'b',  '(quit)' -> 'q'
+  let p = {
     type: 'list',
     name: 'vote',
     message: 'What do you like better?',
     choices: ['(quit)', 'cats', 'dogs'],
-    filter: val => val === '(quit)' ? 'q' : val === 'cats' ? 'a' : 'b'
+    filter: val => ( val === '(quit)' ? 'q' : ( val === 'cats' ? 'a' : 'b' ) )
   }
-  let a = await inquirer.prompt(q)
+  let a = await inquirer.prompt(p)
   if (a.vote === 'q') process.exit()
-  let res = await request.post(voteURL, { json: a }).json
-  console.log(voteToString(res.result))
+  try {
+    let res = await request.post(voteURL, { json: a }).json
+    console.log(voteToString(res.result))
+  } catch (err) {
+    exit(1, 'command "vote" %s', err.message)
+  }
 }
 
-async function onResults(cmd, opts) {
-  let res = await request.get(resultsURL).json
-  console.log(tallyToString(res.result))
+// Handle the results command and submit request to API.
+async function doResultsCmd(cmd, opts) {
+  try {
+    let res = await request.get(resultsURL).json
+    console.log(tallyToString(res.result))
+  } catch (err) {
+    exit(1, 'command "results" %s', err.message)
+  }
 }
 
+// Pretty-print vote.
 function voteToString(vote) {
   if (!vote) return `error: empty vote result`
-  let id = vote.voter_id ? `${vote.voter_id}` : '?'
-  let choice = vote.vote === 'a' ? 'cats' : 'dogs'
+  let id = vote.voter_id ? `${vote.voter_id}` : '-'
+  let choice = ( vote.vote === 'a' ? 'cats' : 'dogs' )
   return `Voter (id: ${id}) voted for: ${choice}`
 }
 
+// Pretty-print vote tally.
 function tallyToString(tally) {
   if (!tally) return `error: empty tally result`
-  let winner = tally.a > tally.b ? 'CATS WIN!' : tally.b > tally.a ? 'DOGS WIN!' : 'IT\'S A TIE!'
-  return `Total votes -> cats: ${tally.a}, dogs: ${tally.b} ... ${winner}`
+  let a = tally.a, b = tally.b
+  let winner = ( a > b  ? 'CATS WIN!' : ( b > a ? 'DOGS WIN!' : 'IT\'S A TIE!' ) )
+  return `Total votes -> cats: ${a}, dogs: ${b} ... ${winner}`
 }
+
+function main() {
+  // Ensure any unhandled promise rejections get logged.
+  process.on('unhandledRejection', error => {
+    logError('(unhandledRejection)', error)
+  })
+
+  // if no args, print help
+  if (!process.argv.slice(2).length) {
+    app.outputHelp()
+    console.log()
+    process.exit()
+  }
+
+  app.version(pkg.version)
+
+  app.command('vote')
+    .description('vote for cats or dogs')
+    .action(doVoteCmd)
+
+  app.command('results')
+    .description('tally the votes')
+    .action(doResultsCmd)
+
+  app.command('*')
+    .action((cmd) => {
+      exit(1, 'unrecognized command ', cmd)
+    })
+
+  // parsing command line args invokes the app handlers
+  try {
+    app.parse(process.argv)
+  } catch (err) {
+    exit(1, err)
+  }
+}
+
+main()
+
