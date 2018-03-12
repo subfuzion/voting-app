ec5182291cd0d6b1efd0fdd100c213d24f730858
Tony Pujals <tony@subfuzion.com>
2018-03-12 02:10:16 -0700

    update tests and config to ensure travis builds exit

--- travis.sh
+++ travis.sh
@@ -1,13 +1,12 @@
 #/bin/sh
 set -e
 
-pkgs="database queue"
+pkgs="database queue worker vote voter"
 
 cd src
 for p in $pkgs; do
     echo "test: $p"
     cd $p
-    docker-compose -f docker-compose.test.yml up
+    [ -f docker-compose.test.yml ] && docker-compose -f docker-compose.test.yml run --rm sut
     cd ..
 done
-
